08ae0c7ace9f0f438a96947bc160a3377cd9233a
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-06-29 23:08:06 -0300

    Adiciona campos created_at e updated_at

--- src/database/migrations/1593398903161-CreateAppointments.ts
+++ src/database/migrations/1593398903161-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1593398903161
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
