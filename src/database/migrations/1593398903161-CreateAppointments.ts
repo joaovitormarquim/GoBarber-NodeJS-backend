a2e5b98de8500ae2c14414255238f0c422c4ac7c
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-06-29 22:58:40 -0300

    Remove key isNullable

--- src/database/migrations/1593398903161-CreateAppointments.ts
+++ src/database/migrations/1593398903161-CreateAppointments.ts
@@ -18,13 +18,11 @@ export default class CreateAppointments1593398903161
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
 
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
