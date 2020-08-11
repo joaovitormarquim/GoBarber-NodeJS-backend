6e4300472f08e430c8170547f59e5736106e9975
jvmsmfn <jvmsmfn@cin.ufpe.br>
2020-08-11 10:27:11 -0300

    Creates controllers and routes, typeorm tokens repository and migrations

--- src/shared/infra/typeorm/migrations/1597151501214-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1597151501214-CreateUserTokens.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1593398903161
+export default class CreateUserTokens1597151501214
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'user_tokens',
         columns: [
           {
             name: 'id',
@@ -16,13 +16,15 @@ export default class CreateAppointments1593398903161
           },
 
           {
-            name: 'provider',
-            type: 'varchar',
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
 
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'user_id',
+            type: 'uuid',
           },
 
           {
@@ -37,11 +39,21 @@ export default class CreateAppointments1593398903161
             default: 'now()',
           },
         ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('user_tokens');
   }
 }
