fd66195e6f20f9c0b7282bc55af6a67e0bfef81f
jvmsmfn <jvmsmfn@cin.ufpe.br>
2020-08-15 21:54:06 -0300

    List availability of provider per day

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,63 @@
+// import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+
+    const startHour = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + startHour,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInThisHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInThisHour,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
