cc3f7a1fb8dd0d660298db74bd0e488ab2b2f572
jvmsmfn <jvmsmfn@cin.ufpe.br>
2020-08-16 01:04:02 -0300

    Don not allow appointmens with yourself and create appointments off the schedule

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,6 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
-import { uuid } from 'uuidv4';
 import { startOfHour, isEqual } from 'date-fns';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -22,19 +21,16 @@ describe('CreateAppointment', () => {
     });
 
     const date = new Date(2020, 4, 10, 13);
-    const provider_id = uuid();
-
-    const user_id = uuid();
 
     const appointment = await createAppointment.execute({
       date,
-      provider_id,
-      user_id,
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     await expect(appointment).toHaveProperty('id');
-    await expect(appointment.provider_id).toBe(provider_id);
-    await expect(appointment.user_id).toBe(user_id);
+    await expect(appointment.provider_id).toBe('provider-id');
+    await expect(appointment.user_id).toBe('user-id');
     await expect(isEqual(appointment.date, startOfHour(date))).toBe(true);
   });
 
@@ -47,15 +43,15 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '1231231',
-      user_id: '12312312',
+      provider_id: 'provider-id',
+      user_id: 'user-id',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '1231231',
-        user_id: '1231231',
+        provider_id: 'provider-id',
+        user_id: 'user-id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
@@ -68,8 +64,50 @@ describe('CreateAppointment', () => {
     await expect(
       createAppointment.execute({
         date: new Date(2020, 4, 10, 11),
-        user_id: '2312312',
-        provider_id: '231231',
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 13),
+        provider_id: 'id',
+        user_id: 'id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment before 8am', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 7),
+        provider_id: 'provider_id',
+        user_id: 'user_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 18),
+        provider_id: 'provider_id',
+        user_id: 'user_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
