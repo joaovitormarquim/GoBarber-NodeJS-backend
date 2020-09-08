0e8e8829b692e43209ff257d5d445270f858c581
jvmsmfn <jvmsmfn@cin.ufpe.br>
2020-09-07 22:58:09 -0300

    Serialize appointments

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -44,7 +45,10 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save<Appointment[]>(cacheKey, appointments);
+      await this.cacheProvider.save<Appointment[]>(
+        cacheKey,
+        classToClass(appointments),
+      );
     }
 
     return appointments;
